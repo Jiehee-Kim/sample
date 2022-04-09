6b0da5511015bb609128ac03ff631bd9e0165e74
user <user@DESKTOP-SGQ3N6M>
2022-04-09 10:57:51 +0900

    주석수정

--- src/sample/MainTest.java
+++ src/sample/MainTest.java
@@ -3,7 +3,7 @@ package sample;
 public class MainTest {
 
 	public static void main(String[] args) {
-		//spring에서 추가한 내용
+		//spring에서 추가한 내용1
 		System.out.println("test");
 		
 		//git-hub에서 추가한 내용1
