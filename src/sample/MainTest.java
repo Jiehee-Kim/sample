ecaf78d6cad10e56356b5ecc7efadf87df20f09c
Jiehee-Kim <103294845+Jiehee-Kim@users.noreply.github.com>
2022-04-09 10:56:43 +0900

    Update MainTest.java

--- src/sample/MainTest.java
+++ src/sample/MainTest.java
@@ -3,13 +3,17 @@ package sample;
 public class MainTest {
 
 	public static void main(String[] args) {
+		//spring에서 추가한 내용1
 		System.out.println("test");
-		//git-hub에서 추가한 내용2
+		
+		//git-hub에서 추가한 내용1
 		System.out.println("git-hub");
 		
-		//spring에서 추가한 내용
+		//spring에서 추가한 내용2
 		System.out.println("test2");
 		
+		//git-hub에서 추가한 내용2
+		System.out.println("git-hub2");
 	}
 
 }
