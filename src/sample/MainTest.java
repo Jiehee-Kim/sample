9fd51e8adfe9915c69240373e4f8a3f1d51b2e47
user <user@DESKTOP-SGQ3N6M>
2022-04-09 10:55:39 +0900

    spring에서 내용 추가2

--- src/sample/MainTest.java
+++ src/sample/MainTest.java
@@ -6,7 +6,10 @@ public class MainTest {
 		System.out.println("test");
 		//git-hub에서 추가한 내용2
 		System.out.println("git-hub");
-
+		
+		//spring에서 추가한 내용
+		System.out.println("test2");
+		
 	}
 
 }
