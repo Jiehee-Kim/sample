1db94560279f9bb05e75e200594789a0d372403c
Jiehee-Kim <103294845+Jiehee-Kim@users.noreply.github.com>
2022-04-09 10:46:16 +0900

    Update MainTest.java
    출력문추가

--- src/sample/MainTest.java
+++ src/sample/MainTest.java
@@ -4,7 +4,7 @@ public class MainTest {
 
 	public static void main(String[] args) {
 		System.out.println("test");
-		//git-hub에서 추가한 내용
+		//git-hub에서 추가한 내용2
 		System.out.println("git-hub");
 
 	}
